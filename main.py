9d9c046adefff47131b85f48d5ca27ec3d1b4f2c
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2020-11-02 18:06:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+'''
+Web System Status
+main.py
+
+Remote monitoring tool with web browser GUI and HTTP query mechanism.
+
+2020-10-27
+
+API Endpoints are mapped in /api subdirectory.
+/api/system - System-wide statistics
+/api + /cpu, /ram, /swap, /disk, /net, /boot - Device-specific status
+'''
+
+__author__ = "Giulio Corradini"
+
+
+import http.server
+import argparse
+import sys
+import json
+import stats
+
+
+def jsonify(func):
+    '''
+    Decorator. Dumps as a JSON string, the return value from func
+    :param func: function to decorate
+    :return: callable wrapped function
+    '''
+    def wrapped_func():
+        r = func()
+        return json.dumps(r)
+
+    return wrapped_func
+
+
+class WSSRequestHandler(http.server.SimpleHTTPRequestHandler):
+    routes = {
+        'system': jsonify(stats.system_wide),
+        'cpu': jsonify(stats.cpu),
+        'ram': jsonify(stats.ram),
+        'swap': jsonify(stats.swap),
+        'disk': jsonify(stats.disk),
+        'net': jsonify(stats.net),
+        'boot': jsonify(stats.boot),
+    }
+
+
+    def do_GET(self):
+
+        path = self.path.split(sep='/') # returns ['', 'dir_1'... ]
+        if path[1] == 'api':
+            try:
+                api_endpoint = path[2]
+                action = self.routes.get(api_endpoint)
+
+                if not action:
+                    self.send_response(400)
+                    self.end_headers()
+                    self.wfile.write(b'Invalid endpoint')
+                    return
+
+                response = action()
+
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(response.encode())
+
+            except IndexError:
+                self.send_response(400)
+                self.end_headers()
+                self.wfile.write(b'Endpoint required')
+
+        else:
+            super().do_GET()
+
+
+def main(host, port):
+    with http.server.HTTPServer((host, port), WSSRequestHandler) as server:
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print("Exiting")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", metavar="host", type=str, default='localhost', required=False)
+    parser.add_argument("--port", metavar="port", type=int, default=9999, required=False)
+
+    args = parser.parse_args(sys.argv[1:])
+
+    main(args.host, args.port)
---
+++ stats.py
@@ -0,0 +1,50 @@
+#!$(python3)
+'''
+Web System Status
+stats.py
+
+Routines for system statistics generation
+
+2020-10-27
+'''
+
+__author__ = 'Giulio Corradini'
+
+
+import psutil
+
+
+def cpu():
+    return {"count": psutil.cpu_count(), "load": psutil.getloadavg()}
+
+def ram():
+    return psutil.virtual_memory()
+
+def swap():
+    return psutil.swap_memory()
+
+def disk():
+    return psutil.disk_usage("/")
+
+def net():
+    return psutil.net_io_counters(pernic=True)
+
+def boot():
+    return psutil.boot_time()
+
+
+def system_wide():
+    '''
+    Gets current system status and return a JSON string.
+    :return: str
+    '''
+    statistics = {
+        "cpu": cpu(),
+        "ram": ram(),
+        "swap": swap(),
+        "disk": disk(),
+        "net": net(),
+        "boot": boot()
+    }
+
+    return statistics
